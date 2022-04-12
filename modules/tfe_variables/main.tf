763a996e54dc237bdf9791852184efeb8db680ad
RalphBrynard <ralph@thebrynards.com>
2022-04-12 15:22:53 -0400

    Changed create_variable

--- modules/tfe_variables/main.tf
+++ modules/tfe_variables/main.tf
@@ -1,6 +1,6 @@
 # tfe variables sub-module -- main.tf
 resource "tfe_variable" "this" {
-  count           = var.add_variable ? 1 : 0
+  count           = var.create_variable ? 1 : 0
   key             = var.key
   value           = var.value
   description  = try(trimspace(format("%s %s", var.description, var.description_suffix)), null)
