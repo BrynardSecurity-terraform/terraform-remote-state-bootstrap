152236b7d2190d8058fa0f75e12b537ac0f9ed5d
RalphBrynard <ralph@thebrynards.com>
2022-05-26 09:21:16 -0400

    v0.0.9

--- modules/tfe_variable_set/main.tf
+++ modules/tfe_variable_set/main.tf
@@ -1,7 +1,7 @@
 resource "tfe_variable_set" "this" {
   count         = var.create_variable_set ? 1 : 0
   description   = var.variable_set_description
-  global        = var.global ? false : null
+  global        = var.global
   name          = var.variable_set_name
   organization  = var.organization
   workspace_ids = var.workspace_ids
